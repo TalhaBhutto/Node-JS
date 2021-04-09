0e8f74a95d3ffec1b42864faf9cab2d539827a08
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-09 21:00:23 +0500

    middle ware export

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 const exp=require('express');
 const app=exp()
+const router=exp.Router()
 const checkUrl=(req,res,next)=>{
     console.warn('current route is',req.originalUrl)
     next();
 }
-app.use(checkUrl)
+//app.use(checkUrl)
 app.get('/',(req,res)=>{
     res.send("Hello Express JS")
 })
-app.get('/about',(req,res)=>{
+router.get('/about',checkUrl,(req,res)=>{
     res.send("Welcome to about page")
 })
 app.get('/user',(req,res)=>{
     res.send("Welcome to User page")
 })
+
+app.use('/',router)
 
 app.listen(4500)
