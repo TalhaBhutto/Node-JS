3d649ed125ff62f6a7dea13733168ecadf84706b
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-09 19:41:54 +0500

    Update FileSystem.js

---
+++ FileSystem.js
@@ -0,0 +1,7 @@
+var http=require('http');
+var fs=require('fs')
+http.createServer((req,res)=>{
+fs.readFile('demo.html',(eror,data)=>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+})
+}).listen(4000)
