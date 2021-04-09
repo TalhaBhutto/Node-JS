7eba70e2e2309d67713463f9e45343d199a003b5
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-09 21:46:51 +0500

    using ejs

--- dynamicApp.js
+++ dynamicApp.js
@@ -1,8 +1,12 @@
 const exp=require('express')
 const app=exp()
-
+app.set('view engine','ejs')
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/htmlfiles/home.html")
+    //res.sendFile(__dirname+"/htmlfiles/home.html")
+    res.render('profile')
 })
-
+app.get("/profile/:name",(req,res)=>{
+    //res.sendFile(__dirname+"/htmlfiles/home.html")
+    res.render('profile',{name:req.params.name})
+})
 app.listen(4000)
