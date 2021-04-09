34e8101743c072087bea47828de6b54ea4575343
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-09 18:23:01 +0500

    installed modemailer

--- Node-Mailer.js
+++ Node-Mailer.js
@@ -9,3 +9,17 @@ var transport =nm.createTransport({
         pass:'Bhutto@9'
     }
 })
+var mailOp={
+    from:'talhabhutto420@gmail.com',
+    to:'talhahussain444@gmail.com'
+    ,subject:'kesa dia?',
+    text:'My first email from Node jS'
+}
+transport.sendMail(mailOp,(error,info)=>{
+    if(error){
+        console.warn(error);
+    }
+    else{
+        console.warn('email sent seccessfully\n',info.response)
+    }
+})
