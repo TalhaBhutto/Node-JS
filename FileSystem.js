b54577f092d7843e16c84583fbfe7a79cac10c2e
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-09 19:45:54 +0500

    Read file

--- FileSystem.js
+++ FileSystem.js
@@ -1,7 +1,7 @@
 var http=require('http');
 var fs=require('fs')
 http.createServer((req,res)=>{
-fs.readFile('demo.html',(eror,data)=>{
+fs.readFile('demp.html',(eror,data)=>{
     res.writeHead(200,{'Content-Type':'text/html'})
 })
 }).listen(4000)
