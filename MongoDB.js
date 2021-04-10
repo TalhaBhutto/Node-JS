96cdd7a2a8512c59b785edfc5e769070452db6c3
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-10 20:11:39 +0500

    posting data

--- MongoDB.js
+++ MongoDB.js
@@ -1,11 +1,27 @@
 const exp=require('express');
 const app=exp();
 const mongo= require('mongoose');
+const User=require('./Models/user');
 
-mongo.connect('mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/MyDemoWeb'
+mongo.connect('mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/MyDemoWeb?retryWrites=true&w=majority'
     ,{
     useNewUrlParser:true,
     useUnifiedTopology:true
     }    
-    ).then(()=>console.warn("Mongo connected"))
+    ).then(console.warn('connected'))
+
+User.find({},function(err,user){
+    err?console.warn(err):console.warn(user)
+})
+const data=new User({
+    _id: new mongo.Types.ObjectId(),
+    name:"Adeel",
+    email:"adeelahsan123@gmail.com",
+    address:"Nagan CHowrangi Karachi"
+});
+
+data.save().then((res)=>{
+    console.warn(res)
+})
+.catch(err=>console.warn(err))
 
--- dynamicApp.js
+++ dynamicApp.js
@@ -1,30 +1,57 @@
-const exp=require('express')
-const app=exp()
+//included exporess
+const exp=require('express');
+const app=exp();
 
-app.use('/Assets',exp.static('Assets'))
+//included mongo DB 
+const mongo= require('mongoose');
 
-app.set('view engine','ejs')
+//user.js has the modal which would be used for our database
+const User=require('./Models/user');
 
+//connected mongo DB
+mongo.connect('mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/MyDemoWeb?retryWrites=true&w=majority'
+    ,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+    }    
+    ).then(console.warn('connected'))
+
+
+// including body parser to parse data from post
+var bodyParser=require('body-parser');
+var urlEncoded=bodyParser.urlencoded();
+
+//included CSS for out project
+app.use('/Assets',exp.static('Assets'));
+
+//all 4 components are present in the view folder
+app.set('view engine','ejs');
+
+//created 4 routes
 app.get("/",(req,res)=>{
-    res.render('home')
-})
+    res.render('home');
+});
 
 app.get("/profile/:name",(req,res)=>{
     data={
         email:'test@test.com',
         address:'Khi',
         skills:['Node-js','React-Js','Python']
-    }
-    res.render('profile',{name:req.params.name,info:data})
-})
+    };
+    res.render('profile',{name:req.params.name,info:data});
+});
 
 
 app.get("/about",(rq,rs)=>{
-    rs.render('About')
-})
+    rs.render('About');
+});
 
 app.get("/login",(req,res)=>{
-    res.render('Login')
-})
+    res.render('Login');
+});
+app.post("/login",urlEncoded,(req,res)=>{
+    res.render('home');
+    console.warn(req.body)
+});
 
 app.listen(4000)
