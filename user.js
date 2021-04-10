bc78af031f4dee605d8399085044161a693d85d2
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-10 16:44:08 +0500

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,8 @@
 const mongo=require('mongoose')
+let schema=new mongo.Schema({
+    _id:mongo.Schema.Types.ObjectId(),
+    name:String,
+    email:String,
+    address:String 
+});
+module.exports=mongo.model('user',schema);
